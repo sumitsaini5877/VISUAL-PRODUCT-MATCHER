ae6be38726c1e0c21efa7d11132a725adf6b311a
Sumit Saini <sumitsaini5877@gmail.com>
2025-01-20 19:29:31 +0530

    update visionService

--- src/services/visionService.js
+++ src/services/visionService.js
@@ -1,7 +1,9 @@
 const vision = require("@google-cloud/vision");
-const client = new vision.ImageAnnotatorClient();
+const client = new vision.ImageAnnotatorClient({keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
 
 async function analyzeImage(imagePath) {
+console.log("imagePath in vision",imagePath);
+
   try {
     const [result] = await client.labelDetection(imagePath);
     const labels = result.labelAnnotations;
